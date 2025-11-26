fc7fa28e7fb5be8dc4a9d66182bd6d6a3a483cbc
triromindo <rahmat.sumantri@romindo.net>
2025-11-26 09:58:18 +0700

    relation manager

---
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TreatmentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'treatments';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->maxValue(42949672.95),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('description')
+            ->columns([
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('price'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
