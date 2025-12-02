ada75e08f4099fc0c315213d20c986e4b6dc06e9
triromindo <rahmat.sumantri@romindo.net>
2025-12-02 10:04:02 +0700

    custom data creation process

--- app/Filament/Resources/ToolResource/Pages/CreateTool.php
+++ app/Filament/Resources/ToolResource/Pages/CreateTool.php
@@ -2,18 +2,30 @@
 
 namespace App\Filament\Resources\ToolResource\Pages;
 
-use App\Filament\Resources\ToolResource;
 use Filament\Actions;
+use Illuminate\Database\Eloquent\Model;
+use App\Filament\Resources\ToolResource;
+use App\Filament\Resources\OwnerResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTool extends CreateRecord
 {
     protected static string $resource = ToolResource::class;
 
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $data['name'] = 'Renamed Tool';
+    // protected function mutateFormDataBeforeCreate(array $data): array
+    // {
+    //     $data['name'] = 'Renamed Tool';
 
-        return $data;
-    }
+    //     return $data;
+    // }
+
+    // protected function handleRecordCreation(array $data): Model
+    // {
+    //     OwnerResource::getModel()::create([
+    //         'email' => 'test@email.com',
+    //         'name' => 'test',
+    //         'phone' => 123
+    //     ]);
+    //     return static::getModel()::create($data);
+    // }
 }
